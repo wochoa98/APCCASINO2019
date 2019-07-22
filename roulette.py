c73af0f24bc618f6dc4b1178e6cc48ac5a77ba4b
kryskon <51208504+kryskon@users.noreply.github.com>
2019-07-22 15:34:23 -0400

    Add files via upload

--- roulette.py
+++ roulette.py
@@ -1,10 +1,48 @@
-import random
+'''
+import random, time
 
+class player:
+	a = 0
+	b = 0
+	c = 0
+	d = 0
+	e = 0
+	f = 0
+	g = 0
+	h = 0
+	i = 0
+	j = 0
+	balance = 0
+	playType = 0
+	startingBalance = 0
+	bet = 0
+
+	def __init__(self, num, startingBalance):
+		self.playerNumber = num
+		self.startingBalance = startingBalance
+		self.balance = self.balance
+		self.playType = random.randint(1,3)
+
+p1 = player(1, 1200)
+p2 = player(2, 1500)
+p3 = player(3, 2000)
+p4 = player(4, 2500)
+p5 = player(5, 3000)
+p6 = player(6, 3500)
+
+pL = [p1, p2, p3, p4, p5, p6]
+'''
 class roulette:
 	playerList = []
 	casinoWinnings = 0
 	result = 0
 	tableM = 0
+	if self.tableM == 3:
+		self.tableM = 1000
+	elif self.tableM == 2:
+		self.tableM = 2000
+	elif self.tableM == 1:
+		self.tableM = 3000
 
 	straight_up = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36]
 	first_dozen = [1,2,3,4,5,6,7,8,9,10,11,12]
@@ -36,8 +74,8 @@ class roulette:
 		elif tableMin == 1:
 			self.tableM= 3000
 
-	def __init__(self):
-		self.playerList = []
+	def __init__(self, pL):
+		self.playerList = pL
 
 	def roll(self):
 		self.result = random.choice(self.straight_up)
@@ -57,10 +95,11 @@ class roulette:
 		player.balance = player.startingBalance - player.bet
 		#print("Player", player.playerNumber, "has", player.startingBalance,"dollars and bets", player.bet, "dollars leaving them with", player.balance)
 
-    
-	#Play type 1 will bet on payouts that only give 1:1, 2:1
-	#Play type 2 will bet on payouts such as 5:1, 8:1, 11:1
-	#Play type 3 will bet on payouts such as 11:1, 17:1, 35:1
+	'''
+	Play type 1 will bet on payouts that only give 1:1, 2:1
+	Play type 2 will bet on payouts such as 5:1, 8:1, 11:1
+	Play type 3 will bet on payouts such as 11:1, 17:1, 35:1
+	'''
 
 	def playRound(self):
 
@@ -79,6 +118,44 @@ class roulette:
 			p.i = random.randint(0, 11) #street
 			p.j = random.randint(0, 36) #straight up
 			self.casinoWinnings += p.bet
+			'''
+			if p.playType == 1:
+				if p.bet <= p.startingBalance/3:
+					#print("Player", p.playerNumber, "places their bet on", self.bet_placement1[p.a][0])
+				elif p.bet > p.startingBalance/3 and p.bet < p.startingBalance/1.5:
+					#print("Player", p.playerNumber, "places their bet on", self.bet_placement1[p.a][0] + " and " + self.bet_placement1[p.b][0])
+				elif p.bet >= p.startingBalance/1.5:
+					#print("Player", p.playerNumber, "places their bet on", self.bet_placement1[p.a][0] + ", " + self.bet_placement1[p.b][0], "and", self.bet_placement1[p.c][0])
+			elif p.playType == 2:
+				if self.bet_placement2[p.d][0] == 'line':
+					if p.bet <= p.startingBalance/3:
+						#print("Player", p.playerNumber, "places their bet on", self.bet_placement2[p.d][1][p.g])
+					elif p.bet > p.startingBalance/3 and p.bet < p.startingBalance/1.5:
+						#print("Player", p.playerNumber, "places their bet on", self.bet_placement2[p.d][1][p.g], " and ",  self.bet_placement2[2][1][p.h])
+					elif p.bet >= p.startingBalance/1.5:
+						#print("Player", p.playerNumber, "places their bet on", self.bet_placement2[p.d][1][p.g], ", ", self.bet_placement2[2][1][p.h], "and", self.bet_placement2[3][1][p.i])
+				elif self.bet_placement2[p.d][0] == 'corner':
+					if p.bet <= p.startingBalance/3:
+						#print("Player", p.playerNumber, "places their bet on", self.bet_placement2[p.d][1][p.h])
+					elif p.bet > p.startingBalance/3 and p.bet < p.startingBalance/1.5:
+						#print("Player", p.playerNumber, "places their bet on", self.bet_placement2[p.d][1][p.h], " and ",  self.bet_placement2[3][1][p.i])
+					elif p.bet >= p.startingBalance/1.5:
+						#print("Player", p.playerNumber, "places their bet on", self.bet_placement2[p.d][1][p.h], ", ", self.bet_placement2[3][1][p.i], "and", self.bet_placement2[1][1][p.g])
+				elif self.bet_placement2[p.d][0] == 'street':
+					if p.bet <= p.startingBalance/3:
+						#print("Player", p.playerNumber, "places their bet on", self.bet_placement2[p.d][1][p.i])
+					elif p.bet > p.startingBalance/3 and p.bet < p.startingBalance/1.5:
+						#print("Player", p.playerNumber, "places their bet on", self.bet_placement2[p.d][1][p.i], " and ",  self.bet_placement2[1][1][p.g])
+					elif p.bet >= p.startingBalance/1.5:
+						#print("Player", p.playerNumber, "places their bet on", self.bet_placement2[p.d][1][p.i], ", ", self.bet_placement2[1][1][p.g], "and", self.bet_placement2[2][1][p.h])
+			elif p.playType == 3:
+				if p.bet <= p.startingBalance/3:
+					#print("Player", p.playerNumber, "places their bet on", self.bet_placement3[1][1][p.j])
+				elif p.bet > p.startingBalance/3 and p.bet < p.startingBalance/1.3:
+					#print("Player", p.playerNumber, "places their bet on", self.bet_placement3[2][0])
+				elif p.bet >= p.startingBalance/3:
+					#print("Player", p.playerNumber, "places their bet on", self.bet_placement3[1][1][p.j], ", ", self.bet_placement3[3][1])
+			'''
 			if p.playType == 1:
 				#if p.startingBalance < tableMin:
 					#print("You no longer have funds to play this game")
@@ -372,3 +449,35 @@ class roulette:
 					if self.result != self.bet_placement3[1][1][p.j] and self.result not in self.bet_placement3[3][1]:
 						p.balance = p.balance
 
+						#print("Oh no!", p.playerNumber,"", self.result, "did not land in", self.bet_placement3[1][1][p.j], "or", self.bet_placement3[3][1], "you now have", p.balance, "dollars")
+		print(self.casinoWinnings)
+			#player_bet_balance(p)
+			#bet_places(p)
+
+newGame = roulette(pL)
+newGame.playRound()
+
+'''
+player_bet_balance(p1)
+player_bet_balance(p2)
+player_bet_balance(p3)
+player_bet_balance(p4)
+player_bet_balance(p5)
+player_bet_balance(p6)
+
+bet_places(p1)
+bet_places(p2)
+bet_places(p3)
+bet_places(p4)
+bet_places(p5)
+bet_places(p6)
+
+spin_phrase()
+
+bet(p1)
+bet(p2)
+bet(p3)
+bet(p4)
+bet(p5)
+bet(p6)
+'''
