ec07f3655cb61143d3314c0eda993a12a68c2a08
kryskon <51208504+kryskon@users.noreply.github.com>
2019-07-22 14:13:55 -0400

    Update roulette.py

--- roulette.py
+++ roulette.py
@@ -36,13 +36,7 @@ class roulette:
 	playerList = []
 	casinoWinnings = 0
 	result = 0
-	tableM = 0
-	if self.tableM == 3:
-		self.tableM = 1000
-	elif self.tableM == 2:
-		self.tableM = 2000
-	elif self.tableM == 1:
-		self.tableM = 3000
+	tableM = 3
 
 	straight_up = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36]
 	first_dozen = [1,2,3,4,5,6,7,8,9,10,11,12]
@@ -102,7 +96,12 @@ class roulette:
 	'''
 
 	def playRound(self):
-
+		if self.tableM == 3:
+			self.tableM = 1000
+		elif self.tableM == 2:
+			self.tableM = 2000
+		elif self.tableM == 1:
+			self.tableM = 3000
 		self.roll()
 		for p in self.playerList:
 			p.bet = random.randint(self.tableM, p.startingBalance)
